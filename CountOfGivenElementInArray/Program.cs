16d69ce38d594110df4da641ce88e7311724c8fd
dakata <dakata@students.softuni.bg>
2017-02-26 04:02:52 +0200

    Creating a new project to count elements in array

--- CountOfGivenElementInArray/Program.cs
+++ CountOfGivenElementInArray/Program.cs
@@ -10,6 +10,18 @@ namespace CountOfGivenElementInArray
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
+            var number = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            int[] myArray = input.Split(' ').Select(x => int.Parse(x)).ToArray();
+
+            foreach (var item in myArray)
+            {
+                if (item == number) count++;
+            }
+
+            Console.WriteLine(count);
         }
     }
 }
