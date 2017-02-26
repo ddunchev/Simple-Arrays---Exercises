63d706882ea5e23e9b0c0197e418747fef906976
dakata <dakata@students.softuni.bg>
2017-02-26 16:19:37 +0200

    Adding new project named IncreasingSequenceOfElements

---
+++ IncreasingSequenceOfElements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace IncreasingSequenceOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            int[] myArray = input.Split(' ').Select(x => int.Parse(x)).ToArray();
+            Console.WriteLine(GetIncSeqOfElements(myArray));
+        }
+
+        static string GetIncSeqOfElements(int[] array)
+        {
+            string yesOrNot = "Yes";
+            int minVal = int.MinValue;
+
+            foreach (var item in array)
+            {
+                if (item < minVal) yesOrNot = "No";
+                minVal = item;
+            }
+            return yesOrNot;
+        }
+    }
+}
