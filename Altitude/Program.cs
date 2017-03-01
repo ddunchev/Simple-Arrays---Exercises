f3373b117b5c971e18d06087c695f55c0d552713
dancho <danchodunchev@gmail.com>
2017-03-01 21:45:07 +0200

    Adding a first solution of Altitude project with 70thy percen result

---
+++ Altitude/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altitude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+#if DEBUG
+            Console.SetIn(new System.IO.StreamReader(@"D:\input.txt"));
+#endif
+
+
+            var input = Console.ReadLine();
+
+            string[] myString = input.Split(' ').ToArray();
+
+            string[] upOrDown = new string[myString.Length];
+            int[] high = new int[myString.Length];
+
+            int number = 1, countNum = 0, countString = 0;
+
+            foreach (var item in myString)
+            {
+                if (int.TryParse(item, out number))
+                {
+                    high[countNum] = number;
+                    countNum++;
+                }
+                else
+                {
+                    upOrDown[countString] = item;
+                    countString++;
+                }
+            }
+
+            int altitude = high[0];
+
+            for (int i = 0; i < myString.Length; i++)
+            {
+                if (upOrDown[i] == "up")
+                {
+                    altitude += high[i+1];
+                }
+                else if(upOrDown[i] == "down")
+                {
+                    altitude -= high[i+1];
+                }
+            }
+
+            if (altitude > 0)
+            {
+                Console.WriteLine($"got through safely. current altitude: {altitude}m");
+            }
+            else
+            {
+                Console.WriteLine("crashed");
+            }
+        }
+    }
+}
---
+++ ArraySymmetry/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySymmetry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+#if DEBUG
+            Console.SetIn(new System.IO.StreamReader(@"D:\input.txt"));
+#endif
+
+            var input = Console.ReadLine();
+
+            string[] myArray = input.Split(' ').ToArray();
+
+            int count = 0;
+
+            for (int i = 0; i < myArray.Length/2; i++)
+            {
+                if (myArray[i] == myArray[myArray.Length - 1 - i])
+                {
+                    count += 0;
+                }
+                else count += 1;
+            }
+
+            if (count == 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
