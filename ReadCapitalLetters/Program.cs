a0cfe0a3f4d35006149b270e3c8219de0f3992c5
dancho <danchodunchev@gmail.com>
2017-03-01 19:51:34 +0200

    Adding ReadCapitalLetters project.

---
+++ ReadCapitalLetters/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadCapitalLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+#if DEBUG
+            Console.SetIn(new System.IO.StreamReader(@"D:\input.txt"));
+#endif
+
+
+            var input = Console.ReadLine();
+            int count = 0;
+            char myItem; 
+            foreach (string item in input.Split(' '))
+            {
+                //Console.WriteLine((int)item);
+
+                if (char.TryParse(item, out myItem))
+                {
+                    if ((int)myItem >= 65 & (int)myItem <= 90)
+                    {
+                        count++;
+                    }
+                }
+
+
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
