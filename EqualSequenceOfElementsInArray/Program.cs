de58ea21875274917b4df3f109196cff4371cb13
dancho <danchodunchev@gmail.com>
2017-03-01 19:26:49 +0200

    Add EqualSequenceOfElementsInArray project

---
+++ EqualSequenceOfElementsInArray/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualSequenceOfElementsInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+#if DEBUG
+            Console.SetIn(new System.IO.StreamReader(@"D:\input.txt"));
+#endif
+
+            string input = Console.ReadLine();
+
+            int[] arrayOfNumbers = input.Split(' ').Select(x => int.Parse(x)).ToArray();
+
+            int start = arrayOfNumbers[0];
+            foreach (var item in arrayOfNumbers)
+            {
+                if (item == start)
+                {
+                    start = item;
+                }
+                else start = 0;
+            }
+
+            if (start == arrayOfNumbers[0])
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
